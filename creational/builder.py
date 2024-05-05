9ff9d14ce80ede80d88f70a88a133c0882c464eb
kamalovniyaz <niyaz.kamalov.21@gmail.com>
2024-05-05 23:04:19 +0300

    add builder pattern implementation

--- creational/builder.py
+++ creational/builder.py
@@ -17,3 +17,118 @@ Cоздать объект Дом.
 Чтобы создать объект, вам нужно поочерёдно вызывать методы строителя.
 Причём не нужно запускать все шаги, а только те, что нужны для производства объекта определённой конфигурации.
 """
+from abc import ABC, abstractmethod
+
+
+class Car:
+    pass
+
+
+class Manual:
+    pass
+
+
+class Builder(ABC):
+    @abstractmethod
+    def set_tires(self, tires_model: str) -> None:
+        pass
+
+    @abstractmethod
+    def set_engine(self, engine_name: str) -> None:
+        pass
+
+    @abstractmethod
+    def set_seats(self, seats_count: int) -> None:
+        pass
+
+    @abstractmethod
+    def set_car_radio(self, set_radio) -> bool:
+        pass
+
+
+class CarBuilder(Builder):
+    def __init__(self):
+        self.car = Car()
+
+    def set_tires(self, tires_model: str) -> None:
+        print(f'Установить колеса {tires_model}')
+
+    def set_engine(self, engine_name: str) -> None:
+        print(f'Установить двигатель {engine_name}')
+
+    def set_seats(self, seats_count: int) -> None:
+        print(f'Установить {seats_count} сидения')
+
+    def set_car_radio(self, set_radio: bool) -> None:
+        if set_radio:
+            print("Установить радио в машину")
+        else:
+            print("Не устанавливать радио в машину")
+
+    def get_result(self) -> Car:
+        return self.car
+
+
+class CarManualBuilder(Builder):
+    def __init__(self):
+        self.manual = Manual()
+
+    def set_tires(self, tires_model: str) -> None:
+        print(f'Добавить в руководство информацию о колесах {tires_model}')
+
+    def set_engine(self, engine_model: str) -> None:
+        print(f'Добавить в руководство информацию о двигателе {engine_model}')
+
+    def set_seats(self, seats_model: int) -> None:
+        print(f'Добавить в руководство информацию о {seats_model} сидениях ')
+
+    def set_car_radio(self, set_radio: bool) -> None:
+        if set_radio:
+            print("Добавить в руководство информацию о радио")
+        else:
+            print("Не добавлять в руководство информацию о радио")
+
+    def get_result(self) -> Manual:
+        return self.manual
+
+
+class Director:
+    def create_sport_car(self, builder: Builder):
+        builder.set_tires('MICHELIN Pilot Sport 5 245/40/R18 97Y')
+        builder.set_engine('2JZ-GE')
+        builder.set_seats(2)
+        builder.set_car_radio(False)
+
+    def create_family_car(self, builder: Builder):
+        builder.set_tires('Kumho Ecsta HS52 185/65/R15 88H')
+        builder.set_engine('G4LC')
+        builder.set_seats(4)
+        builder.set_car_radio(True)
+
+
+class Application:
+    @staticmethod
+    def make_car():
+        director = Director()
+
+        print('__________Выпустить семейную машину__________')
+        builder = CarBuilder()
+        director.create_family_car(builder)
+        builder.get_result()
+
+        builder = CarManualBuilder()
+        director.create_family_car(builder)
+        builder.get_result()
+
+        print('__________Выпустить спортивную машину__________')
+        builder = CarBuilder()
+        director.create_sport_car(builder)
+        builder.get_result()
+
+        builder = CarManualBuilder()
+        director.create_sport_car(builder)
+        builder.get_result()
+
+
+if __name__ == '__main__':
+    builder = Application.make_car()
